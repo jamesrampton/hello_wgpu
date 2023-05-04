161a7e45f3a522a3689b17c01f718894c8beeb99
James Rampton <me@jamesrampton.com>
2023-05-04 10:27:34 +0100

    Make it async

--- src/lib.rs
+++ src/lib.rs
@@ -112,7 +112,7 @@ impl State {
 }
 
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
-pub fn run() {
+pub async fn run() {
     cfg_if::cfg_if! {
         if #[cfg(target_arch = "wasm32")] {
             std::panic::set_hook(Box::new(console_error_panic_hook::hook));
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use hello_wgpu::run;
 
 fn main() {
-    run();
+    // TODO it would be nice if I could get this working with tokio, but mio is
+    // complaining so this pollster thing will have to do for now.
+    pollster::block_on(run());
 }
